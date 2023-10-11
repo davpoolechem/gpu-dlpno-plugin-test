b990575139705fd03747c483421d4b8f468724bd
David Poole <dpoole34@gatech.edu>
2023-10-11 11:58:37 -0400

    Starting point of test plugin

---
+++ src/gpu-dlpno.cpp
@@ -0,0 +1,11 @@
+#include "gpu-dlpno.hpp"
+
+namespace gpu_dlpno {
+
+void say_hi() {
+  printf("Hi from David! ๑(◕‿◕)๑ \n");  
+}
+
+} // namespace gpu-dlpno
+
+
---
+++ src/gpu-dlpno.hpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+namespace gpu_dlpno {
+
+void say_hi(); 
+
+} // namespace gpu-dlpno
