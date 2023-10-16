9358180b07ae7bd215e1dc051c2d28555f6ccfac
David Poole <dpoole34@gatech.edu>
2023-10-16 14:11:00 -0400

    Remove pybind stuff for now

--- src/test.cpp
+++ src/test.cpp
@@ -1,13 +1,13 @@
 #include "test.hpp"
 
-#include <pybind11/pybind11.h>
+//#include <pybind11/pybind11.h>
 
 void say_hi() {
   printf("Hi from David in C++! ๑(◕‿◕)๑ \n");  
 }
 
-PYBIND11_MODULE(gpu_dlpnoccsd, m) {
-    m.def("say_hi", &say_hi, "Say hi from C++!");
-}
+//PYBIND11_MODULE(gpu_dlpnoccsd, m) {
+//    m.def("say_hi", &say_hi, "Say hi from C++!");
+//}
 
 
