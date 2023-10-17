cf12d950c4be82cbcf603b3c3a3b5e15f9f4fa70
David Poole <dpoole34@gatech.edu>
2023-10-17 11:34:50 -0400

    Some module fixups

--- src/cxx_module.cpp
+++ src/cxx_module.cpp
@@ -15,14 +15,14 @@ using namespace pybind11::literals;
 namespace psi {
 namespace dlpno {
 
-void export_gpu_dlpnoccsd(py::module& m) {
+void export_gpu_dlpno_ccsd(py::module& m) {
     m.def("say_hi", &say_hi, "Says hi from C++!");
 }
 
-PYBIND11_MODULE(gpu_dlpnoccsd, m) 
+PYBIND11_MODULE(gpu_dlpno_ccsd, m) 
 {
-    m.doc() = "Python API of gpu_dlpnoccsd: a GPU-accelerated DLPNO-CCSD plugin to Psi4";
-    export_gpu_dlpnoccsd(m);
+    m.doc() = "Python API of gpu_dlpno_ccsd: a GPU-accelerated DLPNO-CCSD plugin to Psi4";
+    export_gpu_dlpno_ccsd(m);
 }
 
 } // namespace dlpno
--- src/test.hpp
+++ src/test.hpp
@@ -1,4 +1,12 @@
 #include <psi4/libfock/jk.h>
 #include <stdio.h>
 
+namespace psi {
+namespace dlpno {
+
 void say_hi();
+
+} // namespace dlpno
+} // namespace psi
+
+
